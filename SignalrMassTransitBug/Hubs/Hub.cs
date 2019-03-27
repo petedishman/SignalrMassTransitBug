667b47ede2b08764676d126e1538ab79c16e5a8e
Pete Dishman <peter.dishman@netcall.com>
2019-03-27 21:04:12 +0000

    make minimal asp.net/signalr test app

---
+++ SignalrMassTransitBug/Hubs/Hub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SignalrMassTransitBug.Hubs
+{
+    public class TestHub : Hub
+    {
+        public async Task Test()
+        {
+            try
+            {
+                await Clients.All.TestResponse($"{DateTime.Now} - Hello");
+            }
+            catch (Exception ex)
+            {
+                await Clients.All.TestResponse($"{DateTime.Now} - Got exception:\n{ex.ToString()}");
+            }            
+        }
+
+        private async void TestAsyncVoid()
+        {
+
+        }
+    }
+}
